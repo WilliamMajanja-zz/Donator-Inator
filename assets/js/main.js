4e83a92e36e54a5ff4316f0505f86d5d512a7622
William Majanja <williammajanja@Gmail.com>
2021-09-22 01:09:36 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,36 @@
+var PesaPal = require('pesapaljs').init({
+    key: CONSUMER_KEY,
+    secret: CONSUMER_SECRET,
+    debug: true // false in production!
+});
+
+// Listen for IPNs (With an express app)
+app.get('/ipn', PesaPal.paymentListener, function(req, res) { 
+    var payment = req.payment;
+    // do shit with payment {transaction, method, status, reference}
+
+    // DO NOT res.send()
+});
+
+var options = {
+    reference: "42314123", // Send this
+    transaction: "175c6485-0948-4cb9-8d72-05a2c3f25be5" // or both.
+};
+PesaPal.getPaymentStatus(options, function(error, status}{
+    // do shit
+});
+
+PesaPal.getPaymentDetails(options, function (error, payment) {
+    //payment -> {transaction, method, status, reference}
+    //do shit
+});
+var customer = new PesaPal.Customer("kariuki@pesapal.com");
+var order = new PesaPal.Order("42314123", customer, "Ma ndazi", 1679.50, "KES", "MERCHANT");
+
+// Redirect user to PesaPal
+var url = PesaPal.getPaymentURL(order, "http://mysite.co.ke/callback");
+// send it to an iframe ?
+
 jQuery(document).ready(function($) {
 
     /* ======= Scrollspy ======= */
